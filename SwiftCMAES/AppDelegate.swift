cd2569aaf3601da1b1f779b614efaf085f53e0c4
Santiago Gonzalez <slgonzalez@me.com>
2019-04-13 12:20:49 -0500

    Initial Commit

---
+++ SwiftCMAES/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftCMAES
+//
+//  Created by Santiago Gonzalez on 4/13/19.
+//  Copyright © 2019 Santiago Gonzalez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ SwiftCMAES/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftCMAES
+//
+//  Created by Santiago Gonzalez on 4/13/19.
+//  Copyright © 2019 Santiago Gonzalez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
